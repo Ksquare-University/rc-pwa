a242225a4f3b01206f2b4185a4345755f558efb2
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2023-01-19 18:36:33 -0600

    Added home components and user location identify

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 
 // This component gives us the pages general layout and allows us to connect components through the outlet
 const Layout = () => {
-  <div>Hola mundo</div>;
   return <Outlet />;
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,9 @@
 // import { selectUser, selectToken } from "../features/authSlice";
 // import { auth } from "../firebaseConfig";
 // import { useNavigate } from "react-router-dom";
+import CarouselComponent from "../components/Carousel";
+import Footer from "../components/Footer";
+import HomeBanner from "../components/HomeBanner";
 import { useAuth } from "../context/AuthCtx";
 import "../styles/home.css";
 
@@ -14,7 +17,7 @@ const Home = (props: Props) => {
   // const email = useAppSelector(selectEmail);
   // const user = useAppSelector(selectUser);
   // const token = useAppSelector(selectToken);
-  const { user, logOut, loading } = useAuth(); // contiene el contexto de autenticación, podemos acceder a sus props
+  const { logOut, loading } = useAuth(); // contiene el contexto de autenticación, podemos acceder a sus props
 
   const handleLogout = async () => {
     await logOut();
@@ -24,37 +27,11 @@ const Home = (props: Props) => {
     return <h1>Loading ....</h1>;
   }
 
-  const welcome = user ? `Hola, ${user.displayName}!` : "Bievenid@!!!";
-
-  console.log(user.accessToken); // para facilitar el copy paste en postman
-
   return (
     <div className="home-container">
-      <div className="content">
-        <h2>Restaurantes a Domicilio</h2>
-        <h1>{welcome}</h1>
-        <div className="info">
-          <img alt="" />
-        </div>
-        <div className="info">
-          <p>User ID:</p>
-          <span>{user.uid}</span>
-        </div>
-        <div className="info">
-          <p>Email:</p> <span>{user.email}</span>
-        </div>
-        <div className="info">
-          <p>
-            Rol: <br />
-          </p>{" "}
-          <span>
-            {user.reloadUserInfo.customAttributes &&
-              user.reloadUserInfo.customAttributes.split('"')[3]}
-          </span>
-        </div>
-
-        <button onClick={handleLogout}>Salir</button>
-      </div>
+      <HomeBanner />
+      <CarouselComponent />
+      <Footer />
     </div>
   );
 };
